bbc22956b5294fff71e1305d9a3d7564ebb8d194
LETALNIK <nikita1216289@email.com>
2024-10-05 11:36:25 +0300

    12345678

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ def Beauty(hop):
         return 0
     if hop == 1:
             return "P"
-map[2][3] = 1
 
 def DisplayMap(map):
     for i in range(8):
-        print(Beauty(map[i][0]), " ", Beauty(map[i][1]), " ", Beauty(map[i][2]), " ", Beauty(map[i][3]), " ", Beauty(map[i][4]), " ", Beauty(map[i][5]), " ", Beauty(map[i][6]), " ", Beauty(map[i][7]))
+        for j in range(8):
+            print(Beauty(map[i][j]), end=' ')
+        print(end = "\n")
 
+def MakeMove(id, map):
 DisplayMap(map)
+
+
 
