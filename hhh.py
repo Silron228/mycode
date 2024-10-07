2b2968453109a89942ed28b03e869fe7a1760ae1
Inflyser <89538138944s@gmail.com>
2024-10-07 14:05:47 +0300

    att5

--- hhh.py
+++ hhh.py
@@ -9,28 +9,25 @@ knights_figur, bishops_figur, rooks_figur = [], [], []
 king_figur, queen_figur = [], []
 
 all_figur = [pawns_figur, knights_figur, bishops_figur, rooks_figur, king_figur, queen_figur]
-    
-def constructor(name, color, frame):
-    return (name, color, frame)
 
-def flag(col):
-    if col == "white": return white_frame
-    else: return black_frame
-    
-def index_frame(n):
-    return algebrs.index(n)
-
-def create_figur(listas: list, colors, name, amount):
-    for i in range(amount):
-        listas.append((name, colors, flag(colors)[index_frame(name)]))
-
-def create_two(coler):
-    for i in range(len(algebrs)):
-        create_figur(all_figur[i], coler, algebrs[i], number[i])
- 
 def create():
-    create_two("white"), create_two("black")
+    def flag(col):
+        if col == "white": return white_frame
+        else: return black_frame
 
+    def index_frame(n):
+        return algebrs.index(n)
+
+    def create_figur(listas: list, colors, name, amount):
+        for i in range(amount):
+            listas.append((name, colors, flag(colors)[index_frame(name)]))
+
+    def create_two(coler):
+        for i in range(len(algebrs)):
+            create_figur(all_figur[i], coler, algebrs[i], number[i])
+       
+    create_two("white"), create_two("black")
+  
 if __name__ == "__main__":
     create()
     
