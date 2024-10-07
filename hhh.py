9894622330d4c424724c60c3bbcc3ebd490a2ce1
Inflyser <89538138944s@gmail.com>
2024-10-07 14:02:59 +0300

    att4

--- hhh.py
+++ hhh.py
@@ -9,15 +9,9 @@ knights_figur, bishops_figur, rooks_figur = [], [], []
 king_figur, queen_figur = [], []
 
 all_figur = [pawns_figur, knights_figur, bishops_figur, rooks_figur, king_figur, queen_figur]
-
-class Figur():
-    def __init__(self, name, color, frame) -> None:
-        self.name = name
-        self.color = color
-        self.frame = frame
     
-    def constructor(self):
-        return (self.name, self.color, self.frame)
+def constructor(name, color, frame):
+    return (name, color, frame)
 
 def flag(col):
     if col == "white": return white_frame
@@ -28,11 +22,11 @@ def index_frame(n):
 
 def create_figur(listas: list, colors, name, amount):
     for i in range(amount):
-        listas.append(Figur(name, colors, flag(colors)[index_frame(name)]).constructor())
+        listas.append((name, colors, flag(colors)[index_frame(name)]))
 
 def create_two(coler):
     for i in range(len(algebrs)):
-        create_figur(all_figur[i], coler, algebrs[i], number[0])
+        create_figur(all_figur[i], coler, algebrs[i], number[i])
  
 def create():
     create_two("white"), create_two("black")
@@ -41,4 +35,6 @@ if __name__ == "__main__":
     create()
     
     print(all_figur)
+    
+    
     
