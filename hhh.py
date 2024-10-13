152ed670a418c8f4776d13b946ea8f36151eddb4
Inflyser <89538138944s@gmail.com>
2024-10-13 12:59:22 +0300

    att22

--- hhh.py
+++ hhh.py
@@ -1,7 +1,7 @@
 # ШАХМАТЫ
 import copy
 
-white_frame = ["♙", "♞", "♝", "♜", "♛", "♚"]
+white_frame = ["♟", "♞", "♝", "♜", "♛", "♚"]
 black_frame = ["♙", '♘', "♗", "♖", "♕", "♔"]
 names = ["pawn", "knight", "bishop", "rook", "queen", "king"]
 amount = [8, 2, 2, 2, 1, 1]
@@ -132,14 +132,14 @@ def handler(move):
                 try:
                     x1 = alf.index(move_hand[0])+1
                     y1 = numder.index(move_hand[1])+1           
-                except ValueError: moves()
+                except ValueError: not_move()
         
                 if len(move) == 5:
                     # куда
                     try:
                         x2 = alf.index(move_hand[2])+1
                         y2 = numder.index(move_hand[3])+1
-                    except ValueError: moves()
+                    except ValueError: not_move()
 
                     if desk[y1][x1] == block[0] or \
                         desk[y1][x1]== block[1]:
@@ -155,20 +155,19 @@ def handler(move):
     
 def moves():
     global mov, y1, x1, y2, x2
-    # e4-e5 or E4-E5
     try:
         mov = handler(str(input("-")))
     except KeyboardInterrupt: print(" game off"), exit()
     
     y1, x1 = mov[0], mov[1] 
     y2, x2 = mov[2], mov[3]
-    
-    # пешка
+
     def true_move(pawn, k):
         desk[y2][x2] = white_frame[k]
         pawn[-1] = [y2, x2]
         desk[y1][x1] = desk_archive[y1][x1]
         pawn[-2] = True
+        print("успешный ход")
 
     def move_pawn():
         for pawn in pawns_figur: 
@@ -180,7 +179,7 @@ def moves():
                                 true_move(pawn, 0)
                                 break
                         else: not_move()
-                    elif x1 == x2 and (y1 == y2 + 1):
+                    elif x1 == x2 and y1 == y2 + 1:
                         true_move(pawn,0)
                         break
                     else: not_move()
@@ -194,7 +193,7 @@ def moves():
                     if desk[y2][x2] not in white_frame:
                         return True
                     else: not_move()
-        
+
         def move_bishop():
             global resultat, k, result
             resultat = "X"    
@@ -214,22 +213,20 @@ def moves():
                             result.append(False)                       
                         k+=1 
                  
-                    if result.count(False) == 2 and result.count(True) == 1:
-                        resultat = True  
+                    if result.count(False) == 2 and result.count(True) == 1: resultat = True
                     elif result.count(False) > 2: resultat = False               
-                    else: resultat = True    
+                    else: resultat = True
                 
                 if (y1 == y2 + i and x1 == x2 + i): instakl("+", "+")
                 if (y1 == y2 - i and x1 == x2 + i): instakl("-", "+")
                 if (y1 == y2 + i and x1 == x2 - i): instakl("+", "-")
-                if (y1 == y2 - i and x1 == x2 - i): instakl("-", "-")
-                
-            if resultat == " ": print("не работает")
-            return resultat              
+                if (y1 == y2 - i and x1 == x2 - i): instakl("-", "-")               
+            return resultat   
+                   
                   
         def move_dauble_figur(clas_figur, type_figur):
             for cl in clas_figur:
-                if (desk[y2][x2] in (block+black_frame) and (desk[y2][x2] not in white_frame)):
+                if (desk[y2][x2] in (block+black_frame)):
                     if str([y1]+[x1]) == str(cl[-1]):
                         if type_figur == True:
                             true_move(cl, frame)
@@ -249,18 +246,19 @@ def moves():
     
 def print_desk(): [print(" ".join(i)) for i in desk]
 
-def not_move(): print("not move"), moves()
+def not_move(): play()
 
 def print_all_figur():
     [print(i) for i in pawns_figur], [print("\n")]
     [print(i) for i in knights_figur], [print("\n")]
     [print(i) for i in bishops_figur]
+    
+def play():
+    while True:
+        print_desk()
+        moves()
 
-  
 if __name__ == "__main__":
     create_figur(), create_desk(), setting()
     print_desk()
-    while True:
-        moves()
-        print_all_figur()
-        print_desk()
+    play()      
